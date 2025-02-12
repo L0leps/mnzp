67333e1c5d2f4cd53d530fb5ce3f4d0b6a02c995
L0leps <kiklogigu@gmail.com>
2025-02-12 23:02:03 +0200

    Исправления в mnzp2.py

--- mznp1.py
+++ mznp1.py
@@ -1,8 +1,9 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+print('HELLO WORLD')
+print('HELLO WOR2LD')
 # Load example dataset
-tips = sns.load_dataset("tips")
+tips = sns.load_dataset("ti")
 
 # Create a scatter plot
 sns.scatterplot(x="total_bill", y="tip", hue="day", data=tips)
