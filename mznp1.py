50f8af24c982a59bbb49a32bcb8006a33ce2619b
L0leps <kiklogigu@gmail.com>
2025-02-13 23:37:32 +0200

    i make this shit nrxt time

--- mznp1.py
+++ mznp1.py
@@ -1,12 +1,29 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-print('HELLO WORLD')
-print('HELLO WOR2LD')
-# Load example dataset
-tips = sns.load_dataset("ti")
+import pandas as pd
+import statistics as stats
 
-# Create a scatter plot
-sns.scatterplot(x="total_bill", y="tip", hue="day", data=tips)
+# Завантаження файлу (замінити 'file.csv' на назву твого файлу)
+df = pd.read_csv('laba1.csv')
 
-# Show the plot
+print(df.head())
+
+print(df.info())
+print(df.describe())
+
+
+mean_value = stats.mean(df['Базисний період'])
+median_value = stats.median(df['Базисний період'])
+mode_value = stats.mode(df['Базисний період'])
+
+print(f"Mean: {mean_value}, Median: {median_value}, Mode: {mode_value}")
+variance = stats.pvariance(df['Базисний період'])  # Дисперсія
+std_dev = stats.pstdev(df['Базисний період'])      # Стандартне відхилення
+
+print(f"Variance: {variance}, Standard deviation: {std_dev}")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.histplot(df[' Базисний період'], bins=20, kde=True)
+plt.title('Гістограма для вибраних даних')
 plt.show()
